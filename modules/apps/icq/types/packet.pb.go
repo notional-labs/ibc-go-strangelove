cosmos/ibc-go

Add support for batch requests in single packet

modules/apps/icq/types/packet.pb.go
@@ -26,7 +26,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // InterchainQueryPacketData is comprised of an ABCI query request and type of query.
 type InterchainQueryPacketData struct {
-	Request types.RequestQuery `protobuf:"bytes,1,opt,name=request,proto3" json:"request"`
+	Requests []types.RequestQuery `protobuf:"bytes,1,rep,name=requests,proto3" json:"requests"`
 }
 
 func (m *InterchainQueryPacketData) Reset()         { *m = InterchainQueryPacketData{} }
@@ -62,16 +62,16 @@ func (m *InterchainQueryPacketData) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_InterchainQueryPacketData proto.InternalMessageInfo
 
-func (m *InterchainQueryPacketData) GetRequest() types.RequestQuery {
+func (m *InterchainQueryPacketData) GetRequests() []types.RequestQuery {
 	if m != nil {
-		return m.Request
+		return m.Requests
 	}
-	return types.RequestQuery{}
+	return nil
 }
 
 // InterchainQueryPacketAck is comprised of an ABCI query response with non-deterministic fields left empty (e.g. Codespace, Log, Info and ...).
 type InterchainQueryPacketAck struct {
-	Response types.ResponseQuery `protobuf:"bytes,1,opt,name=response,proto3" json:"response"`
+	Responses []types.ResponseQuery `protobuf:"bytes,1,rep,name=responses,proto3" json:"responses"`
 }
 
 func (m *InterchainQueryPacketAck) Reset()         { *m = InterchainQueryPacketAck{} }
@@ -107,11 +107,11 @@ func (m *InterchainQueryPacketAck) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_InterchainQueryPacketAck proto.InternalMessageInfo
 
-func (m *InterchainQueryPacketAck) GetResponse() types.ResponseQuery {
+func (m *InterchainQueryPacketAck) GetResponses() []types.ResponseQuery {
 	if m != nil {
-		return m.Response
+		return m.Responses
 	}
-	return types.ResponseQuery{}
+	return nil
 }
 
 func init() {
@@ -124,25 +124,25 @@ func init() {
 }
 
 var fileDescriptor_841c9ad988725ce1 = []byte{
-	// 284 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xbf, 0x4a, 0x33, 0x41,
-	0x14, 0x47, 0x77, 0xe1, 0xe3, 0x53, 0xd6, 0x2e, 0x08, 0xc6, 0x88, 0xa3, 0x04, 0x0b, 0x1b, 0x67,
-	0x48, 0x52, 0x0b, 0x1a, 0x6c, 0x04, 0x0b, 0x4d, 0x19, 0x6c, 0x66, 0x6e, 0x86, 0xcd, 0x25, 0xd9,
-	0xb9, 0x93, 0x99, 0xd9, 0x85, 0xbc, 0x85, 0x8f, 0x95, 0x32, 0xa5, 0x95, 0xc8, 0xee, 0x8b, 0xc8,
-	0xee, 0xfa, 0x27, 0x45, 0xba, 0x81, 0x7b, 0x7e, 0x67, 0xe0, 0x24, 0x57, 0xa8, 0x40, 0x48, 0x6b,
-	0x97, 0x08, 0x32, 0x20, 0x19, 0x2f, 0x10, 0x56, 0xa2, 0x18, 0x08, 0x2b, 0x61, 0xa1, 0x03, 0xb7,
-	0x8e, 0x02, 0x75, 0x4e, 0x50, 0x01, 0xdf, 0xa5, 0x38, 0xc2, 0x8a, 0x17, 0x83, 0xde, 0x71, 0x4a,
-	0x29, 0x35, 0x8c, 0xa8, 0x5f, 0x2d, 0xde, 0x3b, 0x0b, 0xda, 0xcc, 0xb4, 0xcb, 0xd0, 0x04, 0x21,
-	0x15, 0xa0, 0x08, 0x6b, 0xab, 0x7d, 0x7b, 0xec, 0x4f, 0x93, 0xd3, 0x47, 0x13, 0xb4, 0x83, 0xb9,
-	0x44, 0xf3, 0x92, 0x6b, 0xb7, 0x7e, 0x6e, 0xbe, 0x7a, 0x90, 0x41, 0x76, 0x6e, 0x93, 0x03, 0xa7,
-	0x57, 0xb9, 0xf6, 0xa1, 0x1b, 0x5f, 0xc6, 0xd7, 0x47, 0xc3, 0x73, 0xfe, 0xe7, 0xe2, 0xb5, 0x8b,
-	0x4f, 0xda, 0x7b, 0xb3, 0x1c, 0xff, 0xdb, 0x7c, 0x5c, 0x44, 0x93, 0x9f, 0x4d, 0xff, 0x35, 0xe9,
-	0xee, 0x75, 0xdf, 0xc3, 0xa2, 0x73, 0x97, 0x1c, 0x3a, 0xed, 0x2d, 0x19, 0xaf, 0xbf, 0xdd, 0x6c,
-	0x8f, 0xbb, 0x05, 0x76, 0xe5, 0xbf, 0xab, 0xf1, 0xd3, 0xa6, 0x64, 0xf1, 0xb6, 0x64, 0xf1, 0x67,
-	0xc9, 0xe2, 0xb7, 0x8a, 0x45, 0xdb, 0x8a, 0x45, 0xef, 0x15, 0x8b, 0xa6, 0xc3, 0x14, 0xc3, 0x3c,
-	0x57, 0x1c, 0x28, 0x13, 0x40, 0x3e, 0x23, 0x2f, 0x50, 0xc1, 0x4d, 0x4a, 0xa2, 0x18, 0x89, 0x8c,
-	0x66, 0xf9, 0x52, 0xfb, 0xba, 0x72, 0x5b, 0xb7, 0xa9, 0xa1, 0xfe, 0x37, 0x39, 0x46, 0x5f, 0x01,
-	0x00, 0x00, 0xff, 0xff, 0x5e, 0x5b, 0x6d, 0x28, 0x82, 0x01, 0x00, 0x00,
+	// 283 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xc1, 0x4a, 0xc3, 0x30,
+	0x18, 0xc7, 0x5b, 0x14, 0xd1, 0x7a, 0x2b, 0x82, 0x73, 0x62, 0x94, 0xe1, 0xc1, 0x8b, 0x09, 0xdb,
+	0x1e, 0x40, 0x2c, 0x5e, 0x04, 0x0f, 0xba, 0xa3, 0x88, 0x90, 0x7c, 0x0b, 0x5d, 0xd8, 0x9a, 0x2f,
+	0x4d, 0xd2, 0xc2, 0xde, 0xc2, 0xc7, 0xda, 0x71, 0x47, 0x4f, 0x22, 0xed, 0x8b, 0x48, 0x5b, 0xdc,
+	0x76, 0xd8, 0x2d, 0x90, 0xdf, 0xff, 0xf7, 0xc1, 0x2f, 0xba, 0x55, 0x02, 0x18, 0x37, 0x66, 0xa1,
+	0x80, 0x7b, 0x85, 0xda, 0x31, 0x05, 0x39, 0x2b, 0x87, 0xcc, 0x70, 0x98, 0x4b, 0x4f, 0x8d, 0x45,
+	0x8f, 0xf1, 0xb9, 0x12, 0x40, 0x77, 0x29, 0xaa, 0x20, 0xa7, 0xe5, 0xb0, 0x7f, 0x96, 0x62, 0x8a,
+	0x2d, 0xc3, 0x9a, 0x57, 0x87, 0xf7, 0x2f, 0xbd, 0xd4, 0x53, 0x69, 0x33, 0xa5, 0x3d, 0xe3, 0x02,
+	0x14, 0xf3, 0x4b, 0x23, 0x5d, 0xf7, 0x39, 0xf8, 0x88, 0x2e, 0x9e, 0xb5, 0x97, 0x16, 0x66, 0x5c,
+	0xe9, 0xb7, 0x42, 0xda, 0xe5, 0x6b, 0x7b, 0xea, 0x89, 0x7b, 0x1e, 0x3f, 0x44, 0xc7, 0x56, 0xe6,
+	0x85, 0x74, 0xde, 0xf5, 0xc2, 0x9b, 0x83, 0xbb, 0xd3, 0xd1, 0x15, 0xdd, 0xca, 0x68, 0x23, 0xa3,
+	0x93, 0x0e, 0x68, 0xa7, 0xc9, 0xe1, 0xea, 0xe7, 0x3a, 0x98, 0x6c, 0x46, 0x83, 0xcf, 0xa8, 0xb7,
+	0xd7, 0xfe, 0x08, 0xf3, 0x38, 0x89, 0x4e, 0xac, 0x74, 0x06, 0xb5, 0x93, 0xff, 0x76, 0xb2, 0xc7,
+	0xde, 0x11, 0xbb, 0xfa, 0xed, 0x2c, 0x79, 0x59, 0x55, 0x24, 0x5c, 0x57, 0x24, 0xfc, 0xad, 0x48,
+	0xf8, 0x55, 0x93, 0x60, 0x5d, 0x93, 0xe0, 0xbb, 0x26, 0xc1, 0xfb, 0x28, 0x55, 0x7e, 0x56, 0x08,
+	0x0a, 0x98, 0x31, 0x40, 0x97, 0xa1, 0x63, 0x4a, 0xc0, 0x7d, 0x8a, 0xac, 0x1c, 0xb3, 0x0c, 0xa7,
+	0xc5, 0x42, 0xba, 0xa6, 0x74, 0x57, 0xb8, 0x2d, 0x22, 0x8e, 0xda, 0x24, 0xe3, 0xbf, 0x00, 0x00,
+	0x00, 0xff, 0xff, 0xcf, 0x68, 0x77, 0x4c, 0x86, 0x01, 0x00, 0x00,
 }
 
 func (m *InterchainQueryPacketData) Marshal() (dAtA []byte, err error) {
@@ -165,16 +165,20 @@ func (m *InterchainQueryPacketData) MarshalToSizedBuffer(dAtA []byte) (int, erro
 	_ = i
 	var l int
 	_ = l
-	{
-		size, err := m.Request.MarshalToSizedBuffer(dAtA[:i])
-		if err != nil {
-			return 0, err
+	if len(m.Requests) > 0 {
+		for iNdEx := len(m.Requests) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Requests[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintPacket(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
 		}
-		i -= size
-		i = encodeVarintPacket(dAtA, i, uint64(size))
 	}
-	i--
-	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -198,16 +202,20 @@ func (m *InterchainQueryPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error
 	_ = i
 	var l int
 	_ = l
-	{
-		size, err := m.Response.MarshalToSizedBuffer(dAtA[:i])
-		if err != nil {
-			return 0, err
+	if len(m.Responses) > 0 {
+		for iNdEx := len(m.Responses) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Responses[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintPacket(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
 		}
-		i -= size
-		i = encodeVarintPacket(dAtA, i, uint64(size))
 	}
-	i--
-	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -228,8 +236,12 @@ func (m *InterchainQueryPacketData) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = m.Request.Size()
-	n += 1 + l + sovPacket(uint64(l))
+	if len(m.Requests) > 0 {
+		for _, e := range m.Requests {
+			l = e.Size()
+			n += 1 + l + sovPacket(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -239,8 +251,12 @@ func (m *InterchainQueryPacketAck) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = m.Response.Size()
-	n += 1 + l + sovPacket(uint64(l))
+	if len(m.Responses) > 0 {
+		for _, e := range m.Responses {
+			l = e.Size()
+			n += 1 + l + sovPacket(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -281,7 +297,7 @@ func (m *InterchainQueryPacketData) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Request", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Requests", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -308,7 +324,8 @@ func (m *InterchainQueryPacketData) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.Request.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.Requests = append(m.Requests, types.RequestQuery{})
+			if err := m.Requests[len(m.Requests)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -364,7 +381,7 @@ func (m *InterchainQueryPacketAck) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Response", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Responses", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -391,7 +408,8 @@ func (m *InterchainQueryPacketAck) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.Response.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.Responses = append(m.Responses, types.ResponseQuery{})
+			if err := m.Responses[len(m.Responses)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex

notional-labs/ibc-go-strangelove

modules/apps/icq/types/packet.pb.go
@@ -26,7 +26,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // InterchainQueryPacketData is comprised of an ABCI query request and type of query.
 type InterchainQueryPacketData struct {
-	Request types.RequestQuery `protobuf:"bytes,1,opt,name=request,proto3" json:"request"`
+	Requests []types.RequestQuery `protobuf:"bytes,1,rep,name=requests,proto3" json:"requests"`
 }
 
 func (m *InterchainQueryPacketData) Reset()         { *m = InterchainQueryPacketData{} }
@@ -62,16 +62,16 @@ func (m *InterchainQueryPacketData) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_InterchainQueryPacketData proto.InternalMessageInfo
 
-func (m *InterchainQueryPacketData) GetRequest() types.RequestQuery {
+func (m *InterchainQueryPacketData) GetRequests() []types.RequestQuery {
 	if m != nil {
-		return m.Request
+		return m.Requests
 	}
-	return types.RequestQuery{}
+	return nil
 }
 
 // InterchainQueryPacketAck is comprised of an ABCI query response with non-deterministic fields left empty (e.g. Codespace, Log, Info and ...).
 type InterchainQueryPacketAck struct {
-	Response types.ResponseQuery `protobuf:"bytes,1,opt,name=response,proto3" json:"response"`
+	Responses []types.ResponseQuery `protobuf:"bytes,1,rep,name=responses,proto3" json:"responses"`
 }
 
 func (m *InterchainQueryPacketAck) Reset()         { *m = InterchainQueryPacketAck{} }
@@ -107,11 +107,11 @@ func (m *InterchainQueryPacketAck) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_InterchainQueryPacketAck proto.InternalMessageInfo
 
-func (m *InterchainQueryPacketAck) GetResponse() types.ResponseQuery {
+func (m *InterchainQueryPacketAck) GetResponses() []types.ResponseQuery {
 	if m != nil {
-		return m.Response
+		return m.Responses
 	}
-	return types.ResponseQuery{}
+	return nil
 }
 
 func init() {
@@ -124,25 +124,25 @@ func init() {
 }
 
 var fileDescriptor_841c9ad988725ce1 = []byte{
-	// 285 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0xbf, 0x4a, 0x33, 0x41,
-	0x14, 0x47, 0x77, 0xe1, 0xe3, 0x53, 0xd6, 0x2e, 0x08, 0xc6, 0x88, 0xa3, 0x04, 0x0b, 0x1b, 0x67,
-	0x48, 0x04, 0x3b, 0x41, 0x83, 0x8d, 0x60, 0xa1, 0x29, 0x83, 0xcd, 0xcc, 0xcd, 0xb0, 0xb9, 0x24,
-	0x3b, 0x77, 0x32, 0x33, 0xbb, 0x90, 0xb7, 0xf0, 0xb1, 0x52, 0xa6, 0xb4, 0x12, 0xd9, 0x7d, 0x11,
-	0xd9, 0x5d, 0xff, 0xa4, 0x48, 0x37, 0x70, 0xcf, 0xef, 0x0c, 0x9c, 0xe4, 0x02, 0x15, 0x08, 0x69,
-	0xed, 0x02, 0x41, 0x06, 0x24, 0xe3, 0x05, 0xc2, 0x52, 0x14, 0x03, 0x61, 0x25, 0xcc, 0x75, 0xe0,
-	0xd6, 0x51, 0xa0, 0xce, 0x11, 0x2a, 0xe0, 0xdb, 0x14, 0x47, 0x58, 0xf2, 0x62, 0xd0, 0x3b, 0x4c,
-	0x29, 0xa5, 0x86, 0x11, 0xf5, 0xab, 0xc5, 0x7b, 0x27, 0x41, 0x9b, 0xa9, 0x76, 0x19, 0x9a, 0x20,
-	0xa4, 0x02, 0x14, 0x61, 0x65, 0xb5, 0x6f, 0x8f, 0xfd, 0x49, 0x72, 0xfc, 0x68, 0x82, 0x76, 0x30,
-	0x93, 0x68, 0x5e, 0x72, 0xed, 0x56, 0xcf, 0xcd, 0x57, 0x0f, 0x32, 0xc8, 0xce, 0x6d, 0xb2, 0xe7,
-	0xf4, 0x32, 0xd7, 0x3e, 0x74, 0xe3, 0xf3, 0xf8, 0xf2, 0x60, 0x78, 0xca, 0xff, 0x5c, 0xbc, 0x76,
-	0xf1, 0x71, 0x7b, 0x6f, 0x96, 0xa3, 0x7f, 0xeb, 0x8f, 0xb3, 0x68, 0xfc, 0xb3, 0xe9, 0xbf, 0x26,
-	0xdd, 0x9d, 0xee, 0x7b, 0x98, 0x77, 0xee, 0x92, 0x7d, 0xa7, 0xbd, 0x25, 0xe3, 0xf5, 0xb7, 0x9b,
-	0xed, 0x70, 0xb7, 0xc0, 0xb6, 0xfc, 0x77, 0x35, 0x7a, 0x5a, 0x97, 0x2c, 0xde, 0x94, 0x2c, 0xfe,
-	0x2c, 0x59, 0xfc, 0x56, 0xb1, 0x68, 0x53, 0xb1, 0xe8, 0xbd, 0x62, 0xd1, 0x64, 0x98, 0x62, 0x98,
-	0xe5, 0x8a, 0x03, 0x65, 0x02, 0xc8, 0x67, 0xe4, 0x05, 0x2a, 0xb8, 0x4a, 0x49, 0x14, 0x37, 0x22,
-	0xa3, 0x69, 0xbe, 0xd0, 0xbe, 0xae, 0xdc, 0xd6, 0x6d, 0x6a, 0xa8, 0xff, 0x4d, 0x8e, 0xeb, 0xaf,
-	0x00, 0x00, 0x00, 0xff, 0xff, 0xde, 0x44, 0x3d, 0xa8, 0x82, 0x01, 0x00, 0x00,
+	// 283 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x6c, 0x90, 0x41, 0x4b, 0xc3, 0x30,
+	0x18, 0x86, 0x5b, 0x14, 0xd1, 0x7a, 0x2b, 0x82, 0x73, 0x62, 0x94, 0xe1, 0xc1, 0x8b, 0x09, 0x9b,
+	0xe0, 0x55, 0x2c, 0x5e, 0x04, 0x0f, 0xba, 0xa3, 0x88, 0x90, 0x7c, 0x0b, 0x5d, 0xd8, 0x9a, 0x2f,
+	0x4d, 0xd2, 0xc2, 0xfe, 0x85, 0x3f, 0x6b, 0xc7, 0x1d, 0x3d, 0x89, 0xb4, 0x7f, 0x44, 0xda, 0xe2,
+	0xb6, 0xc3, 0x6e, 0x81, 0x3c, 0xef, 0xf3, 0xc1, 0x13, 0x5d, 0x2b, 0x01, 0x8c, 0x1b, 0x33, 0x57,
+	0xc0, 0xbd, 0x42, 0xed, 0x98, 0x82, 0x9c, 0x95, 0x43, 0x66, 0x38, 0xcc, 0xa4, 0xa7, 0xc6, 0xa2,
+	0xc7, 0xf8, 0x54, 0x09, 0xa0, 0xdb, 0x14, 0x55, 0x90, 0xd3, 0x72, 0xd8, 0x3f, 0x49, 0x31, 0xc5,
+	0x96, 0x61, 0xcd, 0xab, 0xc3, 0xfb, 0xe7, 0x5e, 0xea, 0x89, 0xb4, 0x99, 0xd2, 0x9e, 0x71, 0x01,
+	0x8a, 0xf9, 0x85, 0x91, 0xae, 0xfb, 0x1c, 0x7c, 0x44, 0x67, 0xcf, 0xda, 0x4b, 0x0b, 0x53, 0xae,
+	0xf4, 0x5b, 0x21, 0xed, 0xe2, 0xb5, 0x3d, 0xf5, 0xc4, 0x3d, 0x8f, 0x1f, 0xa2, 0x43, 0x2b, 0xf3,
+	0x42, 0x3a, 0xef, 0x7a, 0xe1, 0xd5, 0xde, 0xcd, 0xf1, 0xe8, 0x82, 0x6e, 0x64, 0xb4, 0x91, 0xd1,
+	0x71, 0x07, 0xb4, 0xd3, 0x64, 0x7f, 0xf9, 0x73, 0x19, 0x8c, 0xd7, 0xa3, 0xc1, 0x67, 0xd4, 0xdb,
+	0x69, 0x7f, 0x84, 0x59, 0x9c, 0x44, 0x47, 0x56, 0x3a, 0x83, 0xda, 0xc9, 0x7f, 0x3b, 0xd9, 0x61,
+	0xef, 0x88, 0x6d, 0xfd, 0x66, 0x96, 0xbc, 0x2c, 0x2b, 0x12, 0xae, 0x2a, 0x12, 0xfe, 0x56, 0x24,
+	0xfc, 0xaa, 0x49, 0xb0, 0xaa, 0x49, 0xf0, 0x5d, 0x93, 0xe0, 0x7d, 0x94, 0x2a, 0x3f, 0x2d, 0x04,
+	0x05, 0xcc, 0x18, 0xa0, 0xcb, 0xd0, 0x31, 0x25, 0xe0, 0x36, 0x45, 0x56, 0xde, 0xb3, 0x0c, 0x27,
+	0xc5, 0x5c, 0xba, 0xa6, 0x74, 0x57, 0xb8, 0x2d, 0x22, 0x0e, 0xda, 0x24, 0x77, 0x7f, 0x01, 0x00,
+	0x00, 0xff, 0xff, 0x4f, 0x77, 0x27, 0xcc, 0x86, 0x01, 0x00, 0x00,
 }
 
 func (m *InterchainQueryPacketData) Marshal() (dAtA []byte, err error) {
@@ -165,16 +165,20 @@ func (m *InterchainQueryPacketData) MarshalToSizedBuffer(dAtA []byte) (int, erro
 	_ = i
 	var l int
 	_ = l
-	{
-		size, err := m.Request.MarshalToSizedBuffer(dAtA[:i])
-		if err != nil {
-			return 0, err
+	if len(m.Requests) > 0 {
+		for iNdEx := len(m.Requests) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Requests[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintPacket(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
 		}
-		i -= size
-		i = encodeVarintPacket(dAtA, i, uint64(size))
 	}
-	i--
-	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -198,16 +202,20 @@ func (m *InterchainQueryPacketAck) MarshalToSizedBuffer(dAtA []byte) (int, error
 	_ = i
 	var l int
 	_ = l
-	{
-		size, err := m.Response.MarshalToSizedBuffer(dAtA[:i])
-		if err != nil {
-			return 0, err
+	if len(m.Responses) > 0 {
+		for iNdEx := len(m.Responses) - 1; iNdEx >= 0; iNdEx-- {
+			{
+				size, err := m.Responses[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				if err != nil {
+					return 0, err
+				}
+				i -= size
+				i = encodeVarintPacket(dAtA, i, uint64(size))
+			}
+			i--
+			dAtA[i] = 0xa
 		}
-		i -= size
-		i = encodeVarintPacket(dAtA, i, uint64(size))
 	}
-	i--
-	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -228,8 +236,12 @@ func (m *InterchainQueryPacketData) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = m.Request.Size()
-	n += 1 + l + sovPacket(uint64(l))
+	if len(m.Requests) > 0 {
+		for _, e := range m.Requests {
+			l = e.Size()
+			n += 1 + l + sovPacket(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -239,8 +251,12 @@ func (m *InterchainQueryPacketAck) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = m.Response.Size()
-	n += 1 + l + sovPacket(uint64(l))
+	if len(m.Responses) > 0 {
+		for _, e := range m.Responses {
+			l = e.Size()
+			n += 1 + l + sovPacket(uint64(l))
+		}
+	}
 	return n
 }
 
@@ -281,7 +297,7 @@ func (m *InterchainQueryPacketData) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Request", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Requests", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -308,7 +324,8 @@ func (m *InterchainQueryPacketData) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.Request.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.Requests = append(m.Requests, types.RequestQuery{})
+			if err := m.Requests[len(m.Requests)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -364,7 +381,7 @@ func (m *InterchainQueryPacketAck) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Response", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Responses", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -391,7 +408,8 @@ func (m *InterchainQueryPacketAck) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.Response.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.Responses = append(m.Responses, types.ResponseQuery{})
+			if err := m.Responses[len(m.Responses)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex