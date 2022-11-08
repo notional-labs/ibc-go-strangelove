cosmos/ibc-go

Migrate icq to mono-module architecture

modules/apps/icq/host_ibc_module.go
@@ -1,31 +1,30 @@
-package host
+package icq
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 
-	"github.com/cosmos/ibc-go/v3/modules/apps/icq/host/keeper"
-	"github.com/cosmos/ibc-go/v3/modules/apps/icq/host/types"
-	icqtypes "github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/keeper"
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	ibcexported "github.com/cosmos/ibc-go/v3/modules/core/exported"
 )
 
-// IBCModule implements the ICS26 interface for interchain accounts host chains
-type IBCModule struct {
+// HostIBCModule implements the ICS26 interface for interchain query host chains
+type HostIBCModule struct {
 	keeper keeper.Keeper
 }
 
 // NewIBCModule creates a new IBCModule given the associated keeper
-func NewIBCModule(k keeper.Keeper) IBCModule {
-	return IBCModule{
+func NewIBCModule(k keeper.Keeper) HostIBCModule {
+	return HostIBCModule{
 		keeper: k,
 	}
 }
 
 // OnChanOpenInit implements the IBCModule interface
-func (im IBCModule) OnChanOpenInit(
+func (im HostIBCModule) OnChanOpenInit(
 	ctx sdk.Context,
 	order channeltypes.Order,
 	connectionHops []string,
@@ -34,12 +33,12 @@ func (im IBCModule) OnChanOpenInit(
 	chanCap *capabilitytypes.Capability,
 	counterparty channeltypes.Counterparty,
 	version string,
-) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
+) (string, error) {
+	return "", sdkerrors.Wrap(types.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
 }
 
 // OnChanOpenTry implements the IBCModule interface
-func (im IBCModule) OnChanOpenTry(
+func (im HostIBCModule) OnChanOpenTry(
 	ctx sdk.Context,
 	order channeltypes.Order,
 	connectionHops []string,
@@ -50,48 +49,48 @@ func (im IBCModule) OnChanOpenTry(
 	counterpartyVersion string,
 ) (string, error) {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return "", types.ErrHostSubModuleDisabled
+		return "", types.ErrHostDisabled
 	}
 
 	return im.keeper.OnChanOpenTry(ctx, order, connectionHops, portID, channelID, chanCap, counterparty, counterpartyVersion)
 }
 
 // OnChanOpenAck implements the IBCModule interface
-func (im IBCModule) OnChanOpenAck(
+func (im HostIBCModule) OnChanOpenAck(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 	counterpartyChannelID string,
 	counterpartyVersion string,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
 }
 
 // OnChanOpenAck implements the IBCModule interface
-func (im IBCModule) OnChanOpenConfirm(
+func (im HostIBCModule) OnChanOpenConfirm(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 ) error {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return types.ErrHostSubModuleDisabled
+		return types.ErrHostDisabled
 	}
 
 	return im.keeper.OnChanOpenConfirm(ctx, portID, channelID)
 }
 
 // OnChanCloseInit implements the IBCModule interface
-func (im IBCModule) OnChanCloseInit(
+func (im HostIBCModule) OnChanCloseInit(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 ) error {
-	// Disallow user-initiated channel closing for interchain account channels
+	// Disallow user-initiated channel closing for interchain query channels
 	return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "user cannot close channel")
 }
 
 // OnChanCloseConfirm implements the IBCModule interface
-func (im IBCModule) OnChanCloseConfirm(
+func (im HostIBCModule) OnChanCloseConfirm(
 	ctx sdk.Context,
 	portID,
 	channelID string,
@@ -100,13 +99,13 @@ func (im IBCModule) OnChanCloseConfirm(
 }
 
 // OnRecvPacket implements the IBCModule interface
-func (im IBCModule) OnRecvPacket(
+func (im HostIBCModule) OnRecvPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	_ sdk.AccAddress,
 ) ibcexported.Acknowledgement {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return types.NewErrorAcknowledgement(types.ErrHostSubModuleDisabled)
+		return types.NewErrorAcknowledgement(types.ErrHostDisabled)
 	}
 
 	txResponse, err := im.keeper.OnRecvPacket(ctx, packet)
@@ -122,20 +121,20 @@ func (im IBCModule) OnRecvPacket(
 }
 
 // OnAcknowledgementPacket implements the IBCModule interface
-func (im IBCModule) OnAcknowledgementPacket(
+func (im HostIBCModule) OnAcknowledgementPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	acknowledgement []byte,
 	relayer sdk.AccAddress,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "cannot receive acknowledgement on a host channel end, a host chain does not send a packet over the channel")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "cannot receive acknowledgement on a host channel end, a host chain does not send a packet over the channel")
 }
 
 // OnTimeoutPacket implements the IBCModule interface
-func (im IBCModule) OnTimeoutPacket(
+func (im HostIBCModule) OnTimeoutPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "cannot cause a packet timeout on a host channel end, a host chain does not send a packet over the channel")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "cannot cause a packet timeout on a host channel end, a host chain does not send a packet over the channel")
 }

modules/apps/icq/keeper/genesis.go
@@ -1,0 +1,33 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
+)
+
+// InitGenesis initializes the ibc-transfer state and binds to PortID.
+func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
+	// Only try to bind to port if it is not already bound, since we may already own
+	// port capability from capability InitGenesis
+	if !k.IsBound(ctx, state.HostPort) {
+		// transfer module binds to the transfer port on InitChain
+		// and claims the returned capability
+		err := k.BindPort(ctx, state.HostPort)
+		if err != nil {
+			panic(fmt.Sprintf("could not claim port capability: %v", err))
+		}
+	}
+
+	k.SetParams(ctx, state.Params)
+}
+
+// ExportGenesis exports ibc-transfer module's portID and denom trace info into its genesis state.
+func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
+	return &types.GenesisState{
+		HostPort: types.PortID,
+		Params:   k.GetParams(ctx),
+	}
+}

modules/apps/icq/keeper/grpc_query.go
@@ -5,7 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/cosmos/ibc-go/v3/modules/apps/icq/host/types"
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
 )
 
 var _ types.QueryServer = Keeper{}

modules/apps/icq/keeper/keeper.go
@@ -8,8 +8,7 @@ import (
 	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	icqtypes "github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
-	"github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
 	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
 	"github.com/tendermint/tendermint/libs/log"
 )
@@ -20,18 +19,19 @@ type Keeper struct {
 	cdc        codec.BinaryCodec
 	paramSpace paramtypes.Subspace
 
-	channelKeeper icqtypes.ChannelKeeper
-	portKeeper    icqtypes.PortKeeper
+	ics4Wrapper   types.ICS4Wrapper
+	channelKeeper types.ChannelKeeper
+	portKeeper    types.PortKeeper
 
 	scopedKeeper capabilitykeeper.ScopedKeeper
 
 	querier sdk.Queryable
 }
 
-// NewKeeper creates a new interchain accounts host Keeper instance
+// NewKeeper creates a new interchain query Keeper instance
 func NewKeeper(
 	cdc codec.BinaryCodec, key sdk.StoreKey, paramSpace paramtypes.Subspace,
-	channelKeeper icqtypes.ChannelKeeper, portKeeper icqtypes.PortKeeper,
+	ics4Wrapper types.ICS4Wrapper, channelKeeper types.ChannelKeeper, portKeeper types.PortKeeper,
 	scopedKeeper capabilitykeeper.ScopedKeeper, querier sdk.Queryable,
 ) Keeper {
 	// set KeyTable if it has not already been set
@@ -43,6 +43,7 @@ func NewKeeper(
 		storeKey:      key,
 		cdc:           cdc,
 		paramSpace:    paramSpace,
+		ics4Wrapper:   ics4Wrapper,
 		channelKeeper: channelKeeper,
 		portKeeper:    portKeeper,
 		scopedKeeper:  scopedKeeper,
@@ -52,18 +53,18 @@ func NewKeeper(
 
 // Logger returns the application logger, scoped to the associated module
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
-	return ctx.Logger().With("module", fmt.Sprintf("x/%s-%s", host.ModuleName, icqtypes.ModuleName))
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s-%s", host.ModuleName, types.ModuleName))
 }
 
 // BindPort stores the provided portID and binds to it, returning the associated capability
 func (k Keeper) BindPort(ctx sdk.Context, portID string) *capabilitytypes.Capability {
 	store := ctx.KVStore(k.storeKey)
-	store.Set(icqtypes.KeyPort(portID), []byte{0x01})
+	store.Set(types.KeyPort(portID), []byte{0x01})
 
 	return k.portKeeper.BindPort(ctx, portID)
 }
 
-// IsBound checks if the interchain account host module is already bound to the desired port
+// IsBound checks if the interchain query already bound to the desired port
 func (k Keeper) IsBound(ctx sdk.Context, portID string) bool {
 	_, ok := k.scopedKeeper.GetCapability(ctx, host.PortPath(portID))
 	return ok
@@ -77,4 +78,9 @@ func (k Keeper) AuthenticateCapability(ctx sdk.Context, cap *capabilitytypes.Cap
 // ClaimCapability wraps the scopedKeeper's ClaimCapability function
 func (k Keeper) ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) error {
 	return k.scopedKeeper.ClaimCapability(ctx, cap, name)
+}
+
+// GetAppVersion calls the ICS4Wrapper GetAppVersion function.
+func (k Keeper) GetAppVersion(ctx sdk.Context, portID, channelID string) (string, bool) {
+	return k.ics4Wrapper.GetAppVersion(ctx, portID, channelID)
 }

modules/apps/icq/keeper/relay.go
@@ -3,14 +3,82 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 
-	"github.com/cosmos/ibc-go/v3/modules/apps/icq/host/types"
+	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
 	icqtypes "github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-// OnRecvPacket handles a given interchain accounts packet on a destination host chain.
+func (k Keeper) SendQuery(
+	ctx sdk.Context,
+	sourcePort,
+	sourceChannel string,
+	chanCap *capabilitytypes.Capability,
+	req abci.RequestQuery,
+	timeoutHeight clienttypes.Height,
+	timeoutTimestamp uint64,
+) (uint64, error) {
+	if !k.IsControllerEnabled(ctx) {
+		return 0, types.ErrControllerDisabled
+	}
+
+	sourceChannelEnd, found := k.channelKeeper.GetChannel(ctx, sourcePort, sourceChannel)
+	if !found {
+		return 0, sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", sourcePort, sourceChannel)
+	}
+
+	destinationPort := sourceChannelEnd.GetCounterparty().GetPortID()
+	destinationChannel := sourceChannelEnd.GetCounterparty().GetChannelID()
+
+	icqPacketData := types.InterchainQueryPacketData{
+		Request: req,
+	}
+
+	return k.createOutgoingPacket(ctx, sourcePort, sourceChannel, destinationPort, destinationChannel, chanCap, icqPacketData, timeoutTimestamp)
+}
+
+func (k Keeper) createOutgoingPacket(
+	ctx sdk.Context,
+	sourcePort,
+	sourceChannel,
+	destinationPort,
+	destinationChannel string,
+	chanCap *capabilitytypes.Capability,
+	icqPacketData types.InterchainQueryPacketData,
+	timeoutTimestamp uint64,
+) (uint64, error) {
+	if err := icqPacketData.ValidateBasic(); err != nil {
+		return 0, sdkerrors.Wrap(err, "invalid interchain query packet data")
+	}
+
+	// get the next sequence
+	sequence, found := k.channelKeeper.GetNextSequenceSend(ctx, sourcePort, sourceChannel)
+	if !found {
+		return 0, sdkerrors.Wrapf(channeltypes.ErrSequenceSendNotFound, "failed to retrieve next sequence send for channel %s on port %s", sourceChannel, sourcePort)
+	}
+
+	packet := channeltypes.NewPacket(
+		icqPacketData.GetBytes(),
+		sequence,
+		sourcePort,
+		sourceChannel,
+		destinationPort,
+		destinationChannel,
+		clienttypes.ZeroHeight(),
+		timeoutTimestamp,
+	)
+
+	if err := k.ics4Wrapper.SendPacket(ctx, chanCap, packet); err != nil {
+		return 0, err
+	}
+
+	return packet.Sequence, nil
+}
+
+// OnRecvPacket handles a given interchain queries packet on a destination host chain.
 // If the transaction is successfully executed, the transaction response bytes will be returned.
 func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet) ([]byte, error) {
 	var data icqtypes.InterchainQueryPacketData

notional-labs/ibc-go-strangelove

modules/apps/icq/host_ibc_module.go
@@ -1,31 +1,30 @@
-package host
+package icq
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 
-	"github.com/cosmos/ibc-go/v6/modules/apps/icq/host/keeper"
-	"github.com/cosmos/ibc-go/v6/modules/apps/icq/host/types"
-	icqtypes "github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/keeper"
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
 	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 	ibcexported "github.com/cosmos/ibc-go/v6/modules/core/exported"
 )
 
-// IBCModule implements the ICS26 interface for interchain accounts host chains
-type IBCModule struct {
+// HostIBCModule implements the ICS26 interface for interchain query host chains
+type HostIBCModule struct {
 	keeper keeper.Keeper
 }
 
 // NewIBCModule creates a new IBCModule given the associated keeper
-func NewIBCModule(k keeper.Keeper) IBCModule {
-	return IBCModule{
+func NewIBCModule(k keeper.Keeper) HostIBCModule {
+	return HostIBCModule{
 		keeper: k,
 	}
 }
 
 // OnChanOpenInit implements the IBCModule interface
-func (im IBCModule) OnChanOpenInit(
+func (im HostIBCModule) OnChanOpenInit(
 	ctx sdk.Context,
 	order channeltypes.Order,
 	connectionHops []string,
@@ -34,12 +33,12 @@ func (im IBCModule) OnChanOpenInit(
 	chanCap *capabilitytypes.Capability,
 	counterparty channeltypes.Counterparty,
 	version string,
-) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
+) (string, error) {
+	return "", sdkerrors.Wrap(types.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
 }
 
 // OnChanOpenTry implements the IBCModule interface
-func (im IBCModule) OnChanOpenTry(
+func (im HostIBCModule) OnChanOpenTry(
 	ctx sdk.Context,
 	order channeltypes.Order,
 	connectionHops []string,
@@ -50,48 +49,48 @@ func (im IBCModule) OnChanOpenTry(
 	counterpartyVersion string,
 ) (string, error) {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return "", types.ErrHostSubModuleDisabled
+		return "", types.ErrHostDisabled
 	}
 
 	return im.keeper.OnChanOpenTry(ctx, order, connectionHops, portID, channelID, chanCap, counterparty, counterpartyVersion)
 }
 
 // OnChanOpenAck implements the IBCModule interface
-func (im IBCModule) OnChanOpenAck(
+func (im HostIBCModule) OnChanOpenAck(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 	counterpartyChannelID string,
 	counterpartyVersion string,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "channel handshake must be initiated by controller chain")
 }
 
 // OnChanOpenAck implements the IBCModule interface
-func (im IBCModule) OnChanOpenConfirm(
+func (im HostIBCModule) OnChanOpenConfirm(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 ) error {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return types.ErrHostSubModuleDisabled
+		return types.ErrHostDisabled
 	}
 
 	return im.keeper.OnChanOpenConfirm(ctx, portID, channelID)
 }
 
 // OnChanCloseInit implements the IBCModule interface
-func (im IBCModule) OnChanCloseInit(
+func (im HostIBCModule) OnChanCloseInit(
 	ctx sdk.Context,
 	portID,
 	channelID string,
 ) error {
-	// Disallow user-initiated channel closing for interchain account channels
+	// Disallow user-initiated channel closing for interchain query channels
 	return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "user cannot close channel")
 }
 
 // OnChanCloseConfirm implements the IBCModule interface
-func (im IBCModule) OnChanCloseConfirm(
+func (im HostIBCModule) OnChanCloseConfirm(
 	ctx sdk.Context,
 	portID,
 	channelID string,
@@ -100,13 +99,13 @@ func (im IBCModule) OnChanCloseConfirm(
 }
 
 // OnRecvPacket implements the IBCModule interface
-func (im IBCModule) OnRecvPacket(
+func (im HostIBCModule) OnRecvPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	_ sdk.AccAddress,
 ) ibcexported.Acknowledgement {
 	if !im.keeper.IsHostEnabled(ctx) {
-		return channeltypes.NewErrorAcknowledgement(types.ErrHostSubModuleDisabled)
+		return channeltypes.NewErrorAcknowledgement(types.ErrHostDisabled)
 	}
 
 	txResponse, err := im.keeper.OnRecvPacket(ctx, packet)
@@ -122,20 +121,20 @@ func (im IBCModule) OnRecvPacket(
 }
 
 // OnAcknowledgementPacket implements the IBCModule interface
-func (im IBCModule) OnAcknowledgementPacket(
+func (im HostIBCModule) OnAcknowledgementPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	acknowledgement []byte,
 	relayer sdk.AccAddress,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "cannot receive acknowledgement on a host channel end, a host chain does not send a packet over the channel")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "cannot receive acknowledgement on a host channel end, a host chain does not send a packet over the channel")
 }
 
 // OnTimeoutPacket implements the IBCModule interface
-func (im IBCModule) OnTimeoutPacket(
+func (im HostIBCModule) OnTimeoutPacket(
 	ctx sdk.Context,
 	packet channeltypes.Packet,
 	relayer sdk.AccAddress,
 ) error {
-	return sdkerrors.Wrap(icqtypes.ErrInvalidChannelFlow, "cannot cause a packet timeout on a host channel end, a host chain does not send a packet over the channel")
+	return sdkerrors.Wrap(types.ErrInvalidChannelFlow, "cannot cause a packet timeout on a host channel end, a host chain does not send a packet over the channel")
 }

modules/apps/icq/keeper/genesis.go
@@ -1,0 +1,33 @@
+package keeper
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+)
+
+// InitGenesis initializes the ibc-transfer state and binds to PortID.
+func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
+	// Only try to bind to port if it is not already bound, since we may already own
+	// port capability from capability InitGenesis
+	if !k.IsBound(ctx, state.HostPort) {
+		// transfer module binds to the transfer port on InitChain
+		// and claims the returned capability
+		err := k.BindPort(ctx, state.HostPort)
+		if err != nil {
+			panic(fmt.Sprintf("could not claim port capability: %v", err))
+		}
+	}
+
+	k.SetParams(ctx, state.Params)
+}
+
+// ExportGenesis exports ibc-transfer module's portID and denom trace info into its genesis state.
+func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
+	return &types.GenesisState{
+		HostPort: types.PortID,
+		Params:   k.GetParams(ctx),
+	}
+}

modules/apps/icq/keeper/grpc_query.go
@@ -5,7 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/cosmos/ibc-go/v6/modules/apps/icq/host/types"
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
 )
 
 var _ types.QueryServer = Keeper{}

modules/apps/icq/keeper/keeper.go
@@ -9,8 +9,8 @@ import (
 	capabilitykeeper "github.com/cosmos/cosmos-sdk/x/capability/keeper"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-	icqtypes "github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
-	"github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
 	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
 	"github.com/tendermint/tendermint/libs/log"
 )
@@ -21,18 +21,19 @@ type Keeper struct {
 	cdc        codec.BinaryCodec
 	paramSpace paramtypes.Subspace
 
-	channelKeeper icqtypes.ChannelKeeper
-	portKeeper    icqtypes.PortKeeper
+	ics4Wrapper   porttypes.ICS4Wrapper
+	channelKeeper types.ChannelKeeper
+	portKeeper    types.PortKeeper
 
 	scopedKeeper capabilitykeeper.ScopedKeeper
 
 	querier sdk.Queryable
 }
 
-// NewKeeper creates a new interchain accounts host Keeper instance
+// NewKeeper creates a new interchain query Keeper instance
 func NewKeeper(
 	cdc codec.BinaryCodec, key storetypes.StoreKey, paramSpace paramtypes.Subspace,
-	channelKeeper icqtypes.ChannelKeeper, portKeeper icqtypes.PortKeeper,
+	ics4Wrapper porttypes.ICS4Wrapper, channelKeeper types.ChannelKeeper, portKeeper types.PortKeeper,
 	scopedKeeper capabilitykeeper.ScopedKeeper, querier sdk.Queryable,
 ) Keeper {
 	// set KeyTable if it has not already been set
@@ -44,6 +45,7 @@ func NewKeeper(
 		storeKey:      key,
 		cdc:           cdc,
 		paramSpace:    paramSpace,
+		ics4Wrapper:   ics4Wrapper,
 		channelKeeper: channelKeeper,
 		portKeeper:    portKeeper,
 		scopedKeeper:  scopedKeeper,
@@ -53,18 +55,18 @@ func NewKeeper(
 
 // Logger returns the application logger, scoped to the associated module
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
-	return ctx.Logger().With("module", fmt.Sprintf("x/%s-%s", host.ModuleName, icqtypes.ModuleName))
+	return ctx.Logger().With("module", fmt.Sprintf("x/%s-%s", host.ModuleName, types.ModuleName))
 }
 
 // BindPort stores the provided portID and binds to it, returning the associated capability
 func (k Keeper) BindPort(ctx sdk.Context, portID string) *capabilitytypes.Capability {
 	store := ctx.KVStore(k.storeKey)
-	store.Set(icqtypes.KeyPort(portID), []byte{0x01})
+	store.Set(types.KeyPort(portID), []byte{0x01})
 
 	return k.portKeeper.BindPort(ctx, portID)
 }
 
-// IsBound checks if the interchain account host module is already bound to the desired port
+// IsBound checks if the interchain query already bound to the desired port
 func (k Keeper) IsBound(ctx sdk.Context, portID string) bool {
 	_, ok := k.scopedKeeper.GetCapability(ctx, host.PortPath(portID))
 	return ok
@@ -78,4 +80,9 @@ func (k Keeper) AuthenticateCapability(ctx sdk.Context, cap *capabilitytypes.Cap
 // ClaimCapability wraps the scopedKeeper's ClaimCapability function
 func (k Keeper) ClaimCapability(ctx sdk.Context, cap *capabilitytypes.Capability, name string) error {
 	return k.scopedKeeper.ClaimCapability(ctx, cap, name)
+}
+
+// GetAppVersion calls the ICS4Wrapper GetAppVersion function.
+func (k Keeper) GetAppVersion(ctx sdk.Context, portID, channelID string) (string, bool) {
+	return k.ics4Wrapper.GetAppVersion(ctx, portID, channelID)
 }

modules/apps/icq/keeper/relay.go
@@ -3,14 +3,69 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 
-	"github.com/cosmos/ibc-go/v6/modules/apps/icq/host/types"
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
 	icqtypes "github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
 	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-// OnRecvPacket handles a given interchain accounts packet on a destination host chain.
+func (k Keeper) SendQuery(
+	ctx sdk.Context,
+	sourcePort,
+	sourceChannel string,
+	chanCap *capabilitytypes.Capability,
+	req abci.RequestQuery,
+	timeoutHeight clienttypes.Height,
+	timeoutTimestamp uint64,
+) (uint64, error) {
+	if !k.IsControllerEnabled(ctx) {
+		return 0, types.ErrControllerDisabled
+	}
+
+	sourceChannelEnd, found := k.channelKeeper.GetChannel(ctx, sourcePort, sourceChannel)
+	if !found {
+		return 0, sdkerrors.Wrapf(channeltypes.ErrChannelNotFound, "port ID (%s) channel ID (%s)", sourcePort, sourceChannel)
+	}
+
+	destinationPort := sourceChannelEnd.GetCounterparty().GetPortID()
+	destinationChannel := sourceChannelEnd.GetCounterparty().GetChannelID()
+
+	icqPacketData := types.InterchainQueryPacketData{
+		Request: req,
+	}
+
+	return k.createOutgoingPacket(ctx, sourcePort, sourceChannel, destinationPort, destinationChannel, chanCap, icqPacketData, timeoutTimestamp)
+}
+
+func (k Keeper) createOutgoingPacket(
+	ctx sdk.Context,
+	sourcePort,
+	sourceChannel,
+	destinationPort,
+	destinationChannel string,
+	chanCap *capabilitytypes.Capability,
+	icqPacketData types.InterchainQueryPacketData,
+	timeoutTimestamp uint64,
+) (uint64, error) {
+	if err := icqPacketData.ValidateBasic(); err != nil {
+		return 0, sdkerrors.Wrap(err, "invalid interchain query packet data")
+	}
+
+	return k.ics4Wrapper.SendPacket(
+		ctx,
+		chanCap,
+		sourcePort,
+		sourceChannel,
+		clienttypes.ZeroHeight(),
+		timeoutTimestamp,
+		icqPacketData.GetBytes(),
+	)
+}
+
+// OnRecvPacket handles a given interchain queries packet on a destination host chain.
 // If the transaction is successfully executed, the transaction response bytes will be returned.
 func (k Keeper) OnRecvPacket(ctx sdk.Context, packet channeltypes.Packet) ([]byte, error) {
 	var data icqtypes.InterchainQueryPacketData