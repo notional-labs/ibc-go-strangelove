cosmos/ibc-go

Remove controller side from icq

modules/apps/icq/types/icq.pb.go
@@ -24,10 +24,7 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Params defines the set of on-chain interchain query parameters.
-// The following parameters may be used to disable the host submodule.
 type Params struct {
-	// controller_enabled indicates whether the controller submodules are enabled.
-	ControllerEnabled bool `protobuf:"varint,1,opt,name=controller_enabled,json=controllerEnabled,proto3" json:"controller_enabled,omitempty" yaml:"controller_enabled"`
 	// host_enabled enables or disables the host submodule.
 	HostEnabled bool `protobuf:"varint,2,opt,name=host_enabled,json=hostEnabled,proto3" json:"host_enabled,omitempty" yaml:"host_enabled"`
 	// allow_queries defines a list of query paths allowed to be queried on a host chain.
@@ -67,13 +64,6 @@ func (m *Params) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Params proto.InternalMessageInfo
 
-func (m *Params) GetControllerEnabled() bool {
-	if m != nil {
-		return m.ControllerEnabled
-	}
-	return false
-}
-
 func (m *Params) GetHostEnabled() bool {
 	if m != nil {
 		return m.HostEnabled
@@ -95,26 +85,24 @@ func init() {
 func init() { proto.RegisterFile("ibc/applications/icq/v1/icq.proto", fileDescriptor_733751bafe0a8e23) }
 
 var fileDescriptor_733751bafe0a8e23 = []byte{
-	// 291 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x64, 0x90, 0xb1, 0x4e, 0xc3, 0x30,
-	0x18, 0x84, 0x6b, 0x2a, 0x55, 0x10, 0xca, 0x40, 0xa8, 0xd4, 0x82, 0x84, 0x5b, 0x32, 0x75, 0x21,
-	0x56, 0xe9, 0x56, 0x89, 0xa5, 0x12, 0x5b, 0x07, 0xe8, 0xc8, 0x52, 0xd9, 0xae, 0x95, 0x5a, 0x72,
-	0xf2, 0xa7, 0xb1, 0x13, 0xd4, 0xb7, 0xe0, 0xb1, 0x18, 0xcb, 0xc6, 0x14, 0xa1, 0xe4, 0x0d, 0xf2,
-	0x04, 0x28, 0xb1, 0x80, 0x22, 0x26, 0xfb, 0xfe, 0xfb, 0xee, 0x86, 0x73, 0x6e, 0x24, 0xe3, 0x84,
-	0xc6, 0xb1, 0x92, 0x9c, 0x1a, 0x09, 0x91, 0x26, 0x92, 0x6f, 0x49, 0x36, 0xa9, 0x1f, 0x3f, 0x4e,
-	0xc0, 0x80, 0xdb, 0x97, 0x8c, 0xfb, 0x87, 0x88, 0x5f, 0x7b, 0xd9, 0xe4, 0xaa, 0x17, 0x40, 0x00,
-	0x0d, 0x43, 0xea, 0x9f, 0xc5, 0xbd, 0x77, 0xe4, 0x74, 0x1e, 0x69, 0x42, 0x43, 0xed, 0x2e, 0x1c,
-	0x97, 0x43, 0x64, 0x12, 0x50, 0x4a, 0x24, 0x2b, 0x11, 0x51, 0xa6, 0xc4, 0x7a, 0x80, 0x46, 0x68,
-	0x7c, 0x3c, 0xbf, 0xae, 0xf2, 0xe1, 0xe5, 0x8e, 0x86, 0x6a, 0xe6, 0xfd, 0x67, 0xbc, 0xe5, 0xf9,
-	0xef, 0xf1, 0xc1, 0xde, 0xdc, 0x99, 0xd3, 0xdd, 0x80, 0x36, 0x3f, 0x3d, 0x47, 0x4d, 0x4f, 0xbf,
-	0xca, 0x87, 0x17, 0xb6, 0xe7, 0xd0, 0xf5, 0x96, 0xa7, 0xb5, 0xfc, 0xce, 0xde, 0x3b, 0x67, 0x54,
-	0x29, 0x78, 0x59, 0x6d, 0x53, 0x91, 0x48, 0xa1, 0x07, 0xed, 0x51, 0x7b, 0x7c, 0x32, 0x1f, 0x54,
-	0xf9, 0xb0, 0x67, 0xc3, 0x7f, 0x6c, 0x6f, 0xd9, 0x6d, 0xf4, 0x93, 0x95, 0xf3, 0xc5, 0x5b, 0x81,
-	0xd1, 0xbe, 0xc0, 0xe8, 0xb3, 0xc0, 0xe8, 0xb5, 0xc4, 0xad, 0x7d, 0x89, 0x5b, 0x1f, 0x25, 0x6e,
-	0x3d, 0xdf, 0x05, 0xd2, 0x6c, 0x52, 0xe6, 0x73, 0x08, 0x09, 0x07, 0x1d, 0x82, 0x26, 0x92, 0xf1,
-	0xdb, 0x00, 0x48, 0x36, 0x25, 0x21, 0xac, 0x53, 0x25, 0x74, 0xbd, 0xaf, 0xdd, 0xd5, 0xec, 0x62,
-	0xa1, 0x59, 0xa7, 0x19, 0x6a, 0xfa, 0x15, 0x00, 0x00, 0xff, 0xff, 0xc8, 0x6e, 0x66, 0xa5, 0x7c,
-	0x01, 0x00, 0x00,
+	// 257 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xcc, 0x4c, 0x4a, 0xd6,
+	0x4f, 0x2c, 0x28, 0xc8, 0xc9, 0x4c, 0x4e, 0x2c, 0xc9, 0xcc, 0xcf, 0x2b, 0xd6, 0xcf, 0x4c, 0x2e,
+	0xd4, 0x2f, 0x33, 0x04, 0x51, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xe2, 0x99, 0x49, 0xc9,
+	0x7a, 0xc8, 0x4a, 0xf4, 0x40, 0x72, 0x65, 0x86, 0x52, 0x22, 0xe9, 0xf9, 0xe9, 0xf9, 0x60, 0x35,
+	0xfa, 0x20, 0x16, 0x44, 0xb9, 0x52, 0x33, 0x23, 0x17, 0x5b, 0x40, 0x62, 0x51, 0x62, 0x6e, 0xb1,
+	0x90, 0x15, 0x17, 0x4f, 0x46, 0x7e, 0x71, 0x49, 0x7c, 0x6a, 0x5e, 0x62, 0x52, 0x4e, 0x6a, 0x8a,
+	0x04, 0x93, 0x02, 0xa3, 0x06, 0x87, 0x93, 0xf8, 0xa7, 0x7b, 0xf2, 0xc2, 0x95, 0x89, 0xb9, 0x39,
+	0x56, 0x4a, 0xc8, 0xb2, 0x4a, 0x41, 0xdc, 0x20, 0xae, 0x2b, 0x84, 0x27, 0x64, 0xcb, 0xc5, 0x9b,
+	0x98, 0x93, 0x93, 0x5f, 0x1e, 0x5f, 0x58, 0x9a, 0x5a, 0x94, 0x99, 0x5a, 0x2c, 0xc1, 0xac, 0xc0,
+	0xac, 0xc1, 0xe9, 0x24, 0xf1, 0xe9, 0x9e, 0xbc, 0x08, 0x44, 0x33, 0x8a, 0xb4, 0x52, 0x10, 0x0f,
+	0x98, 0x1f, 0x08, 0xe1, 0x3a, 0xf9, 0x9c, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83,
+	0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43,
+	0x94, 0x51, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e, 0xae, 0x7e, 0x72, 0x7e, 0x71,
+	0x6e, 0x7e, 0xb1, 0x7e, 0x66, 0x52, 0xb2, 0x6e, 0x7a, 0xbe, 0x7e, 0x99, 0xb1, 0x7e, 0x6e, 0x7e,
+	0x4a, 0x69, 0x4e, 0x6a, 0x31, 0x28, 0x44, 0x20, 0x21, 0x51, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4,
+	0x06, 0xf6, 0x9a, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x26, 0x78, 0xd9, 0xdf, 0x2e, 0x01, 0x00,
+	0x00,
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -156,16 +144,6 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x10
 	}
-	if m.ControllerEnabled {
-		i--
-		if m.ControllerEnabled {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x8
-	}
 	return len(dAtA) - i, nil
 }
 
@@ -186,9 +164,6 @@ func (m *Params) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.ControllerEnabled {
-		n += 2
-	}
 	if m.HostEnabled {
 		n += 2
 	}
@@ -236,26 +211,6 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Params: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ControllerEnabled", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowIcq
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.ControllerEnabled = bool(v != 0)
 		case 2:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field HostEnabled", wireType)

notional-labs/ibc-go-strangelove

modules/apps/icq/types/icq.pb.go
@@ -24,10 +24,7 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // Params defines the set of on-chain interchain query parameters.
-// The following parameters may be used to disable the host submodule.
 type Params struct {
-	// controller_enabled indicates whether the controller submodules are enabled.
-	ControllerEnabled bool `protobuf:"varint,1,opt,name=controller_enabled,json=controllerEnabled,proto3" json:"controller_enabled,omitempty" yaml:"controller_enabled"`
 	// host_enabled enables or disables the host submodule.
 	HostEnabled bool `protobuf:"varint,2,opt,name=host_enabled,json=hostEnabled,proto3" json:"host_enabled,omitempty" yaml:"host_enabled"`
 	// allow_queries defines a list of query paths allowed to be queried on a host chain.
@@ -67,13 +64,6 @@ func (m *Params) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_Params proto.InternalMessageInfo
 
-func (m *Params) GetControllerEnabled() bool {
-	if m != nil {
-		return m.ControllerEnabled
-	}
-	return false
-}
-
 func (m *Params) GetHostEnabled() bool {
 	if m != nil {
 		return m.HostEnabled
@@ -95,26 +85,24 @@ func init() {
 func init() { proto.RegisterFile("ibc/applications/icq/v1/icq.proto", fileDescriptor_733751bafe0a8e23) }
 
 var fileDescriptor_733751bafe0a8e23 = []byte{
-	// 292 bytes of a gzipped FileDescriptorProto
+	// 257 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xcc, 0x4c, 0x4a, 0xd6,
 	0x4f, 0x2c, 0x28, 0xc8, 0xc9, 0x4c, 0x4e, 0x2c, 0xc9, 0xcc, 0xcf, 0x2b, 0xd6, 0xcf, 0x4c, 0x2e,
 	0xd4, 0x2f, 0x33, 0x04, 0x51, 0x7a, 0x05, 0x45, 0xf9, 0x25, 0xf9, 0x42, 0xe2, 0x99, 0x49, 0xc9,
 	0x7a, 0xc8, 0x4a, 0xf4, 0x40, 0x72, 0x65, 0x86, 0x52, 0x22, 0xe9, 0xf9, 0xe9, 0xf9, 0x60, 0x35,
-	0xfa, 0x20, 0x16, 0x44, 0xb9, 0xd2, 0x45, 0x46, 0x2e, 0xb6, 0x80, 0xc4, 0xa2, 0xc4, 0xdc, 0x62,
-	0x21, 0x1f, 0x2e, 0xa1, 0xe4, 0xfc, 0xbc, 0x92, 0xa2, 0xfc, 0x9c, 0x9c, 0xd4, 0xa2, 0xf8, 0xd4,
-	0xbc, 0xc4, 0xa4, 0x9c, 0xd4, 0x14, 0x09, 0x46, 0x05, 0x46, 0x0d, 0x0e, 0x27, 0xd9, 0x4f, 0xf7,
-	0xe4, 0x25, 0x2b, 0x13, 0x73, 0x73, 0xac, 0x94, 0x30, 0xd5, 0x28, 0x05, 0x09, 0x22, 0x04, 0x5d,
-	0x21, 0x62, 0x42, 0x56, 0x5c, 0x3c, 0x19, 0xf9, 0xc5, 0x25, 0x70, 0x73, 0x98, 0xc0, 0xe6, 0x88,
-	0x7f, 0xba, 0x27, 0x2f, 0x0c, 0x31, 0x07, 0x59, 0x56, 0x29, 0x88, 0x1b, 0xc4, 0x85, 0xe9, 0xb5,
-	0xe5, 0xe2, 0x4d, 0xcc, 0xc9, 0xc9, 0x2f, 0x8f, 0x2f, 0x2c, 0x4d, 0x2d, 0xca, 0x4c, 0x2d, 0x96,
-	0x60, 0x56, 0x60, 0xd6, 0xe0, 0x74, 0x92, 0xf8, 0x74, 0x4f, 0x5e, 0x04, 0xa2, 0x19, 0x45, 0x5a,
-	0x29, 0x88, 0x07, 0xcc, 0x0f, 0x84, 0x70, 0x9d, 0x7c, 0x4e, 0x3c, 0x92, 0x63, 0xbc, 0xf0, 0x48,
-	0x8e, 0xf1, 0xc1, 0x23, 0x39, 0xc6, 0x09, 0x8f, 0xe5, 0x18, 0x2e, 0x3c, 0x96, 0x63, 0xb8, 0xf1,
-	0x58, 0x8e, 0x21, 0xca, 0x28, 0x3d, 0xb3, 0x24, 0xa3, 0x34, 0x49, 0x2f, 0x39, 0x3f, 0x57, 0x3f,
-	0x39, 0xbf, 0x38, 0x37, 0xbf, 0x58, 0x3f, 0x33, 0x29, 0x59, 0x37, 0x3d, 0x5f, 0xbf, 0xcc, 0x4c,
-	0x3f, 0x37, 0x3f, 0xa5, 0x34, 0x27, 0xb5, 0x18, 0x14, 0xbe, 0x90, 0x70, 0x2d, 0xa9, 0x2c, 0x48,
-	0x2d, 0x4e, 0x62, 0x03, 0x07, 0x94, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x48, 0x71, 0x36, 0x25,
-	0x7c, 0x01, 0x00, 0x00,
+	0xfa, 0x20, 0x16, 0x44, 0xb9, 0x52, 0x33, 0x23, 0x17, 0x5b, 0x40, 0x62, 0x51, 0x62, 0x6e, 0xb1,
+	0x90, 0x15, 0x17, 0x4f, 0x46, 0x7e, 0x71, 0x49, 0x7c, 0x6a, 0x5e, 0x62, 0x52, 0x4e, 0x6a, 0x8a,
+	0x04, 0x93, 0x02, 0xa3, 0x06, 0x87, 0x93, 0xf8, 0xa7, 0x7b, 0xf2, 0xc2, 0x95, 0x89, 0xb9, 0x39,
+	0x56, 0x4a, 0xc8, 0xb2, 0x4a, 0x41, 0xdc, 0x20, 0xae, 0x2b, 0x84, 0x27, 0x64, 0xcb, 0xc5, 0x9b,
+	0x98, 0x93, 0x93, 0x5f, 0x1e, 0x5f, 0x58, 0x9a, 0x5a, 0x94, 0x99, 0x5a, 0x2c, 0xc1, 0xac, 0xc0,
+	0xac, 0xc1, 0xe9, 0x24, 0xf1, 0xe9, 0x9e, 0xbc, 0x08, 0x44, 0x33, 0x8a, 0xb4, 0x52, 0x10, 0x0f,
+	0x98, 0x1f, 0x08, 0xe1, 0x3a, 0xf9, 0x9c, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83,
+	0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb, 0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43,
+	0x94, 0x51, 0x7a, 0x66, 0x49, 0x46, 0x69, 0x92, 0x5e, 0x72, 0x7e, 0xae, 0x7e, 0x72, 0x7e, 0x71,
+	0x6e, 0x7e, 0xb1, 0x7e, 0x66, 0x52, 0xb2, 0x6e, 0x7a, 0xbe, 0x7e, 0x99, 0x99, 0x7e, 0x6e, 0x7e,
+	0x4a, 0x69, 0x4e, 0x6a, 0x31, 0x28, 0x44, 0x20, 0x21, 0x51, 0x52, 0x59, 0x90, 0x5a, 0x9c, 0xc4,
+	0x06, 0xf6, 0x9a, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xa6, 0x67, 0x89, 0x5f, 0x2e, 0x01, 0x00,
+	0x00,
 }
 
 func (m *Params) Marshal() (dAtA []byte, err error) {
@@ -156,16 +144,6 @@ func (m *Params) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i--
 		dAtA[i] = 0x10
 	}
-	if m.ControllerEnabled {
-		i--
-		if m.ControllerEnabled {
-			dAtA[i] = 1
-		} else {
-			dAtA[i] = 0
-		}
-		i--
-		dAtA[i] = 0x8
-	}
 	return len(dAtA) - i, nil
 }
 
@@ -186,9 +164,6 @@ func (m *Params) Size() (n int) {
 	}
 	var l int
 	_ = l
-	if m.ControllerEnabled {
-		n += 2
-	}
 	if m.HostEnabled {
 		n += 2
 	}
@@ -236,26 +211,6 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 			return fmt.Errorf("proto: Params: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
-		case 1:
-			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ControllerEnabled", wireType)
-			}
-			var v int
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowIcq
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				v |= int(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			m.ControllerEnabled = bool(v != 0)
 		case 2:
 			if wireType != 0 {
 				return fmt.Errorf("proto: wrong wireType = %d for field HostEnabled", wireType)