cosmos/ibc-go

Fix port binding error

modules/apps/icq/keeper/genesis.go
@@ -6,17 +6,18 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/cosmos/ibc-go/v3/modules/apps/icq/types"
+	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
 )
 
-// InitGenesis initializes the ibc-transfer state and binds to PortID.
+// InitGenesis initializes the icq state and binds to PortID.
 func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
 	// Only try to bind to port if it is not already bound, since we may already own
 	// port capability from capability InitGenesis
 	if !k.IsBound(ctx, state.HostPort) {
 		// transfer module binds to the transfer port on InitChain
 		// and claims the returned capability
-		err := k.BindPort(ctx, state.HostPort)
-		if err != nil {
+		cap := k.BindPort(ctx, state.HostPort)
+		if err := k.ClaimCapability(ctx, cap, host.PortPath(state.HostPort)); err != nil {
 			panic(fmt.Sprintf("could not claim port capability: %v", err))
 		}
 	}
@@ -24,7 +25,7 @@ func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
 	k.SetParams(ctx, state.Params)
 }
 
-// ExportGenesis exports ibc-transfer module's portID and denom trace info into its genesis state.
+// ExportGenesis exports icq module's portID and denom trace info into its genesis state.
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	return &types.GenesisState{
 		HostPort: types.PortID,

notional-labs/ibc-go-strangelove

modules/apps/icq/keeper/genesis.go
@@ -6,17 +6,18 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	host "github.com/cosmos/ibc-go/v6/modules/core/24-host"
 )
 
-// InitGenesis initializes the ibc-transfer state and binds to PortID.
+// InitGenesis initializes the icq state and binds to PortID.
 func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
 	// Only try to bind to port if it is not already bound, since we may already own
 	// port capability from capability InitGenesis
 	if !k.IsBound(ctx, state.HostPort) {
 		// transfer module binds to the transfer port on InitChain
 		// and claims the returned capability
-		err := k.BindPort(ctx, state.HostPort)
-		if err != nil {
+		cap := k.BindPort(ctx, state.HostPort)
+		if err := k.ClaimCapability(ctx, cap, host.PortPath(state.HostPort)); err != nil {
 			panic(fmt.Sprintf("could not claim port capability: %v", err))
 		}
 	}
@@ -24,7 +25,7 @@ func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
 	k.SetParams(ctx, state.Params)
 }
 
-// ExportGenesis exports ibc-transfer module's portID and denom trace info into its genesis state.
+// ExportGenesis exports icq module's portID and denom trace info into its genesis state.
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	return &types.GenesisState{
 		HostPort: types.PortID,