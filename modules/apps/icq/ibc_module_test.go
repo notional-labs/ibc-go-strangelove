cosmos/ibc-go

updated module test according to new icq packet

modules/apps/icq/ibc_module_test.go
@@ -374,8 +374,10 @@ func (suite *InterchainQueriesTestSuite) TestOnRecvPacket() {
 				},
 			}
 
-			icqPacketData := icqtypes.InterchainQueryPacketData{
-				Requests: requests,
+			bz, err := types.SerializeCosmosQuery(requests)
+			suite.Require().NoError(err)
+			icqPacketData := types.InterchainQueryPacketData{
+				Data: bz,
 			}
 			packetData = icqPacketData.GetBytes()
 
@@ -391,10 +393,13 @@ func (suite *InterchainQueriesTestSuite) TestOnRecvPacket() {
 					Height: resp.Height,
 				}
 			}
+			bz, err = types.SerializeCosmosResponse(resps)
+			suite.Require().NoError(err)
 
-			expectedTxResponse, err := types.ModuleCdc.MarshalJSON(&icqtypes.InterchainQueryPacketAck{
-				Responses: resps,
-			})
+			icqack := icqtypes.InterchainQueryPacketAck{
+				Data: bz,
+			}
+			expectedTxResponse, err := icqtypes.ModuleCdc.MarshalJSON(&icqack)
 			suite.Require().NoError(err)
 
 			expectedAck := channeltypes.NewResultAcknowledgement(expectedTxResponse)

notional-labs/ibc-go-strangelove

modules/apps/icq/ibc_module_test.go
@@ -373,8 +373,10 @@ func (suite *InterchainQueriesTestSuite) TestOnRecvPacket() {
 				},
 			}
 
+			bz, err := types.SerializeCosmosQuery(requests)
+			suite.Require().NoError(err)
 			icqPacketData := types.InterchainQueryPacketData{
-				Requests: requests,
+				Data: bz,
 			}
 			packetData = icqPacketData.GetBytes()
 
@@ -390,10 +392,13 @@ func (suite *InterchainQueriesTestSuite) TestOnRecvPacket() {
 					Height: resp.Height,
 				}
 			}
+			bz, err = types.SerializeCosmosResponse(resps)
+			suite.Require().NoError(err)
 
-			expectedTxResponse, err := types.ModuleCdc.MarshalJSON(&types.InterchainQueryPacketAck{
-				Responses: resps,
-			})
+			icqack := types.InterchainQueryPacketAck{
+				Data: bz,
+			}
+			expectedTxResponse, err := types.ModuleCdc.MarshalJSON(&icqack)
 			suite.Require().NoError(err)
 
 			expectedAck := channeltypes.NewResultAcknowledgement(expectedTxResponse)