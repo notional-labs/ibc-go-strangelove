cosmos/ibc-go

Add unit tests to keeper package

modules/apps/icq/keeper/genesis_test.go
@@ -1,0 +1,46 @@
+package keeper_test
+
+import (
+	"github.com/cosmos/ibc-go/v5/modules/apps/icq/types"
+)
+
+func (suite *KeeperTestSuite) TestInitGenesis() {
+	suite.SetupTest()
+
+	genesisState := types.GenesisState{
+		HostPort: TestPort,
+		Params: types.Params{
+			HostEnabled: false,
+			AllowQueries: []string{
+				"path/to/query1",
+				"path/to/query2",
+			},
+		},
+	}
+
+	suite.chainA.GetSimApp().ICQKeeper.InitGenesis(suite.chainA.GetContext(), genesisState)
+
+	port := suite.chainA.GetSimApp().ICQKeeper.GetPort(suite.chainA.GetContext())
+	suite.Require().Equal(TestPort, port)
+
+	expParams := types.NewParams(
+		false,
+		[]string{
+			"path/to/query1",
+			"path/to/query2",
+		},
+	)
+	params := suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+}
+
+func (suite *KeeperTestSuite) TestExportGenesis() {
+	suite.SetupTest()
+
+	genesisState := suite.chainA.GetSimApp().ICQKeeper.ExportGenesis(suite.chainA.GetContext())
+
+	suite.Require().Equal(types.PortID, genesisState.GetHostPort())
+
+	expParams := types.DefaultParams()
+	suite.Require().Equal(expParams, genesisState.GetParams())
+}

modules/apps/icq/keeper/grpc_query_test.go
@@ -1,0 +1,14 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/icq/types"
+)
+
+func (suite *KeeperTestSuite) TestQueryParams() {
+	ctx := sdk.WrapSDKContext(suite.chainA.GetContext())
+	expParams := types.DefaultParams()
+	res, _ := suite.chainA.GetSimApp().ICQKeeper.Params(ctx, &types.QueryParamsRequest{})
+	suite.Require().Equal(&expParams, res.Params)
+}

modules/apps/icq/keeper/keeper_test.go
@@ -1,0 +1,83 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/icq/types"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v5/testing"
+)
+
+var (
+	TestPort = "icq-test"
+)
+
+type KeeperTestSuite struct {
+	suite.Suite
+
+	coordinator *ibctesting.Coordinator
+
+	// testing chains used for convenience and readability
+	chainA *ibctesting.TestChain
+	chainB *ibctesting.TestChain
+	chainC *ibctesting.TestChain
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 3)
+	suite.chainA = suite.coordinator.GetChain(ibctesting.GetChainID(1))
+	suite.chainB = suite.coordinator.GetChain(ibctesting.GetChainID(2))
+	suite.chainC = suite.coordinator.GetChain(ibctesting.GetChainID(3))
+}
+
+func NewICQPath(chainA, chainB *ibctesting.TestChain) *ibctesting.Path {
+	path := ibctesting.NewPath(chainA, chainB)
+	path.EndpointA.ChannelConfig.PortID = types.PortID
+	path.EndpointB.ChannelConfig.PortID = types.PortID
+	path.EndpointA.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointB.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointA.ChannelConfig.Version = types.Version
+	path.EndpointB.ChannelConfig.Version = types.Version
+
+	return path
+}
+
+// SetupICAPath invokes the ICQ entrypoint and subsequent channel handshake handlers
+func SetupICQPath(path *ibctesting.Path) error {
+	if err := path.EndpointA.ChanOpenInit(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointB.ChanOpenTry(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointA.ChanOpenAck(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointB.ChanOpenConfirm(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (suite *KeeperTestSuite) TestIsBound() {
+	suite.SetupTest()
+
+	path := NewICQPath(suite.chainA, suite.chainB)
+	suite.coordinator.SetupConnections(path)
+
+	err := SetupICQPath(path)
+	suite.Require().NoError(err)
+
+	isBound := suite.chainB.GetSimApp().ICQKeeper.IsBound(suite.chainB.GetContext(), path.EndpointB.ChannelConfig.PortID)
+	suite.Require().True(isBound)
+}

modules/apps/icq/keeper/params_test.go
@@ -1,0 +1,16 @@
+package keeper_test
+
+import "github.com/cosmos/ibc-go/v5/modules/apps/icq/types"
+
+func (suite *KeeperTestSuite) TestParams() {
+	expParams := types.DefaultParams()
+
+	params := suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+
+	expParams.HostEnabled = false
+	expParams.AllowQueries = []string{"/cosmos.staking.v1beta1.MsgDelegate"}
+	suite.chainA.GetSimApp().ICQKeeper.SetParams(suite.chainA.GetContext(), expParams)
+	params = suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+}

modules/apps/icq/keeper/relay_test.go
@@ -1,0 +1,180 @@
+package keeper_test
+
+import (
+	"github.com/cosmos/cosmos-sdk/types/query"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/cosmos/ibc-go/v5/modules/apps/icq/types"
+	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v5/testing"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
+)
+
+func (suite *KeeperTestSuite) TestOnRecvPacket() {
+	var (
+		path       *ibctesting.Path
+		packetData []byte
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"icq successfully queries banktypes.AllBalances",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{
+					Address: suite.chainA.SenderAccount.GetAddress().String(),
+					Pagination: &query.PageRequest{
+						Offset: 0,
+						Limit:  10,
+					},
+				}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path: "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data: suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icqPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icqPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			true,
+		},
+		{
+			"cannot unmarshal interchain query packet data",
+			func() {
+				packetData = []byte{}
+			},
+			false,
+		},
+		{
+			"cannot deserialize interchain query packet data messages",
+			func() {
+				data := []byte("invalid packet data")
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+
+				packetData = icaPacketData.GetBytes()
+			},
+			false,
+		},
+		{
+			"unauthorised: message type not allowed", // NOTE: do not update params to explicitly force the error
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path: "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data: suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+			},
+			false,
+		},
+		{
+			"unauthorised: can not perform historical query (i.e. height != 0)",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path:   "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data:   suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+						Height: 1,
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			false,
+		},
+		{
+			"unauthorised: can not fetch query proof (i.e. prove == true)",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path:  "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data:  suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+						Prove: true,
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.msg, func() {
+			suite.SetupTest() // reset
+
+			path = NewICQPath(suite.chainA, suite.chainB)
+			suite.coordinator.SetupConnections(path)
+
+			err := SetupICQPath(path)
+			suite.Require().NoError(err)
+
+			tc.malleate() // malleate mutates test data
+
+			packet := channeltypes.NewPacket(
+				packetData,
+				suite.chainA.SenderAccount.GetSequence(),
+				path.EndpointA.ChannelConfig.PortID,
+				path.EndpointA.ChannelID,
+				path.EndpointB.ChannelConfig.PortID,
+				path.EndpointB.ChannelID,
+				clienttypes.NewHeight(1, 100),
+				0,
+			)
+
+			txResponse, err := suite.chainB.GetSimApp().ICQKeeper.OnRecvPacket(suite.chainB.GetContext(), packet)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(txResponse)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(txResponse)
+			}
+		})
+	}
+}

notional-labs/ibc-go-strangelove

modules/apps/icq/keeper/genesis_test.go
@@ -1,0 +1,46 @@
+package keeper_test
+
+import (
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+)
+
+func (suite *KeeperTestSuite) TestInitGenesis() {
+	suite.SetupTest()
+
+	genesisState := types.GenesisState{
+		HostPort: TestPort,
+		Params: types.Params{
+			HostEnabled: false,
+			AllowQueries: []string{
+				"path/to/query1",
+				"path/to/query2",
+			},
+		},
+	}
+
+	suite.chainA.GetSimApp().ICQKeeper.InitGenesis(suite.chainA.GetContext(), genesisState)
+
+	port := suite.chainA.GetSimApp().ICQKeeper.GetPort(suite.chainA.GetContext())
+	suite.Require().Equal(TestPort, port)
+
+	expParams := types.NewParams(
+		false,
+		[]string{
+			"path/to/query1",
+			"path/to/query2",
+		},
+	)
+	params := suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+}
+
+func (suite *KeeperTestSuite) TestExportGenesis() {
+	suite.SetupTest()
+
+	genesisState := suite.chainA.GetSimApp().ICQKeeper.ExportGenesis(suite.chainA.GetContext())
+
+	suite.Require().Equal(types.PortID, genesisState.GetHostPort())
+
+	expParams := types.DefaultParams()
+	suite.Require().Equal(expParams, genesisState.GetParams())
+}

modules/apps/icq/keeper/grpc_query_test.go
@@ -1,0 +1,14 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+)
+
+func (suite *KeeperTestSuite) TestQueryParams() {
+	ctx := sdk.WrapSDKContext(suite.chainA.GetContext())
+	expParams := types.DefaultParams()
+	res, _ := suite.chainA.GetSimApp().ICQKeeper.Params(ctx, &types.QueryParamsRequest{})
+	suite.Require().Equal(&expParams, res.Params)
+}

modules/apps/icq/keeper/keeper_test.go
@@ -1,0 +1,83 @@
+package keeper_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+)
+
+var (
+	TestPort = "icq-test"
+)
+
+type KeeperTestSuite struct {
+	suite.Suite
+
+	coordinator *ibctesting.Coordinator
+
+	// testing chains used for convenience and readability
+	chainA *ibctesting.TestChain
+	chainB *ibctesting.TestChain
+	chainC *ibctesting.TestChain
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 3)
+	suite.chainA = suite.coordinator.GetChain(ibctesting.GetChainID(1))
+	suite.chainB = suite.coordinator.GetChain(ibctesting.GetChainID(2))
+	suite.chainC = suite.coordinator.GetChain(ibctesting.GetChainID(3))
+}
+
+func NewICQPath(chainA, chainB *ibctesting.TestChain) *ibctesting.Path {
+	path := ibctesting.NewPath(chainA, chainB)
+	path.EndpointA.ChannelConfig.PortID = types.PortID
+	path.EndpointB.ChannelConfig.PortID = types.PortID
+	path.EndpointA.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointB.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointA.ChannelConfig.Version = types.Version
+	path.EndpointB.ChannelConfig.Version = types.Version
+
+	return path
+}
+
+// SetupICAPath invokes the ICQ entrypoint and subsequent channel handshake handlers
+func SetupICQPath(path *ibctesting.Path) error {
+	if err := path.EndpointA.ChanOpenInit(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointB.ChanOpenTry(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointA.ChanOpenAck(); err != nil {
+		return err
+	}
+
+	if err := path.EndpointB.ChanOpenConfirm(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (suite *KeeperTestSuite) TestIsBound() {
+	suite.SetupTest()
+
+	path := NewICQPath(suite.chainA, suite.chainB)
+	suite.coordinator.SetupConnections(path)
+
+	err := SetupICQPath(path)
+	suite.Require().NoError(err)
+
+	isBound := suite.chainB.GetSimApp().ICQKeeper.IsBound(suite.chainB.GetContext(), path.EndpointB.ChannelConfig.PortID)
+	suite.Require().True(isBound)
+}

modules/apps/icq/keeper/params_test.go
@@ -1,0 +1,16 @@
+package keeper_test
+
+import "github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+
+func (suite *KeeperTestSuite) TestParams() {
+	expParams := types.DefaultParams()
+
+	params := suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+
+	expParams.HostEnabled = false
+	expParams.AllowQueries = []string{"/cosmos.staking.v1beta1.MsgDelegate"}
+	suite.chainA.GetSimApp().ICQKeeper.SetParams(suite.chainA.GetContext(), expParams)
+	params = suite.chainA.GetSimApp().ICQKeeper.GetParams(suite.chainA.GetContext())
+	suite.Require().Equal(expParams, params)
+}

modules/apps/icq/keeper/relay_test.go
@@ -1,0 +1,180 @@
+package keeper_test
+
+import (
+	"github.com/cosmos/cosmos-sdk/types/query"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
+	"github.com/cosmos/ibc-go/v6/modules/apps/icq/types"
+	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	ibctesting "github.com/cosmos/ibc-go/v6/testing"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
+)
+
+func (suite *KeeperTestSuite) TestOnRecvPacket() {
+	var (
+		path       *ibctesting.Path
+		packetData []byte
+	)
+
+	testCases := []struct {
+		msg      string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"icq successfully queries banktypes.AllBalances",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{
+					Address: suite.chainA.SenderAccount.GetAddress().String(),
+					Pagination: &query.PageRequest{
+						Offset: 0,
+						Limit:  10,
+					},
+				}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path: "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data: suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icqPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icqPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			true,
+		},
+		{
+			"cannot unmarshal interchain query packet data",
+			func() {
+				packetData = []byte{}
+			},
+			false,
+		},
+		{
+			"cannot deserialize interchain query packet data messages",
+			func() {
+				data := []byte("invalid packet data")
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+
+				packetData = icaPacketData.GetBytes()
+			},
+			false,
+		},
+		{
+			"unauthorised: message type not allowed", // NOTE: do not update params to explicitly force the error
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path: "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data: suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+			},
+			false,
+		},
+		{
+			"unauthorised: can not perform historical query (i.e. height != 0)",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path:   "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data:   suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+						Height: 1,
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			false,
+		},
+		{
+			"unauthorised: can not fetch query proof (i.e. prove == true)",
+			func() {
+				q := banktypes.QueryAllBalancesRequest{}
+				reqs := []abcitypes.RequestQuery{
+					{
+						Path:  "/cosmos.bank.v1beta1.Query/AllBalances",
+						Data:  suite.chainA.GetSimApp().AppCodec().MustMarshal(&q),
+						Prove: true,
+					},
+				}
+				data, err := types.SerializeCosmosQuery(reqs)
+				suite.Require().NoError(err)
+
+				icaPacketData := types.InterchainQueryPacketData{
+					Data: data,
+				}
+				packetData = icaPacketData.GetBytes()
+
+				params := types.NewParams(true, []string{"/cosmos.bank.v1beta1.Query/AllBalances"})
+				suite.chainB.GetSimApp().ICQKeeper.SetParams(suite.chainB.GetContext(), params)
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		suite.Run(tc.msg, func() {
+			suite.SetupTest() // reset
+
+			path = NewICQPath(suite.chainA, suite.chainB)
+			suite.coordinator.SetupConnections(path)
+
+			err := SetupICQPath(path)
+			suite.Require().NoError(err)
+
+			tc.malleate() // malleate mutates test data
+
+			packet := channeltypes.NewPacket(
+				packetData,
+				suite.chainA.SenderAccount.GetSequence(),
+				path.EndpointA.ChannelConfig.PortID,
+				path.EndpointA.ChannelID,
+				path.EndpointB.ChannelConfig.PortID,
+				path.EndpointB.ChannelID,
+				clienttypes.NewHeight(1, 100),
+				0,
+			)
+
+			txResponse, err := suite.chainB.GetSimApp().ICQKeeper.OnRecvPacket(suite.chainB.GetContext(), packet)
+
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().NotNil(txResponse)
+			} else {
+				suite.Require().Error(err)
+				suite.Require().Nil(txResponse)
+			}
+		})
+	}
+}